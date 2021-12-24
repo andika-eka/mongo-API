9143f8a958d8441214dd99564a542c7b0e8d0b51
andika-eka <putuandika387@gmail.com>
2021-12-24 22:54:32 +0800

    full crud for notification

--- controller/notificationController.js
+++ controller/notificationController.js
@@ -1,5 +1,7 @@
 import Notification from '../models/notification.js'
 
+
+//index
 export const getNotifs = async(req, res) => {
     try {
         const notification = await Notification.find();
@@ -9,6 +11,7 @@ export const getNotifs = async(req, res) => {
     }
 } 
 
+//show
 export const getNotifsID = async(req, res) => {
     try {
         const notification = await Notification.findById(req.params.id);
@@ -18,11 +21,36 @@ export const getNotifsID = async(req, res) => {
     }
 }
 
+//store
 export const storeNotifs = async(req, res) => {
     const notification = new Notification(req.body);
     try {
-        const resave = await notification.save();
-        res.status(200).json(resave);
+        const ressave = await notification.save();
+        res.status(200).json(ressave);
+    } catch (error) {
+        res.status(400).json({message: error.message});
+    }
+}
+
+//update
+export const updateNotifs = async(req, res) => {
+    var notification = await Notification.findById(req.params.id);
+    if  (!notification) return res.status(404).json({status : false, message:"data not found"});
+    try {
+        const ressave = await Notification.updateOne({_id:req.params.id}, {$set:req.body});
+        res.status(200).json(ressave);
+    } catch (error) {
+        res.status(400).json({message: error.message});
+    }
+}
+
+//delete
+export const deleteNotifs = async(req, res) => {
+    var notification = await Notification.findById(req.params.id);
+    if  (!notification) return res.status(404).json({status : false, message:"data not found"});
+    try {
+        const resdel = await Notification.deleteOne({_id:req.params.id});
+        res.status(200).json(resdel);
     } catch (error) {
         res.status(400).json({message: error.message});
     }
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,19 @@
 import express from 'express'
-import { getNotifs, getNotifsID, storeNotifs } from '../controller/notificationController.js'
+import { 
+    getNotifs, 
+    getNotifsID, 
+    storeNotifs,
+    updateNotifs,
+    deleteNotifs
+} from '../controller/notificationController.js'
 
 const router = express.Router()
 
-router.get('/', getNotifs)          //get all
-router.get('/:id', getNotifsID)     //get by ID
-router.post('/', storeNotifs)       //store 
+router.get('/notif/', getNotifs)              //get all
+router.get('/notif/:id', getNotifsID)         //get by ID
+router.post('/notif/', storeNotifs)           //store 
+router.patch('/notif/:id', updateNotifs)      //update by ID
+router.delete('/notif/:id', deleteNotifs)     //delete by ID
 
 
 export default router;
